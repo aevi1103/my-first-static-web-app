89545763a688829609738dc52ef7cd996821df9b
Aebbie Rontos <Aebbie.Rontos@pjws.com>
2022-06-18 12:15:02 -0400

    test new branch

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function Posts() {
-  return <span>Posts</span>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://random-data-api.com/api/cannabis/random_cannabis?size=10")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li>
+          <pre>{JSON.stringify(post, 2, null)}</pre>
+        </li>
+      ))}
+    </ul>
+  );
 }
